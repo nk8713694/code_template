5d4fcbc47b3d6a05fa4e5c85b0a650bea5e86703
Nitish Prasad <nk8713694@gmail.com>
2021-03-20 14:45:17 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<string>
+using namespace std;
+#define fr(i,n) for(i=0;i<n;i++)
+#define far(i,n) for(i=0;i<=n;i++)
+#define fer(i,n) for(i=1;i<n;i++)
+#define max3(a,b,c) max(a,max(b,c))
+#define min3(a,b,c) min(a,min(b,c))
+#define watch(x) cout<<(#x)<<" is "<<(x)<<endl
+#define line(s) getline(cin,s)
+#define mem(a,n) memset(a,n,sizeof(a))
+#define pb push_back
+#define po pop_back
+#define MOD 1000000007
+#define X first
+#define Y second
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef priority_queue<int> pq;
+int main()
+{
+ios::sync_with_stdio(false);
+int t;
+int i,j,n;
+cin>>t;
+while(t--)
+{
+cin>>n;
+}
+return 0;
+}
